2e53e2839363797c33b2042c8489839c4e9fea60
Sanders DeNardi <sedenardi@gmail.com>
2018-08-03 17:37:10 -0400

    add logging support

--- testServer.js
+++ testServer.js
@@ -1,9 +1,13 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const morgan = require('koa-morgan');
+const winston = require('./lib/winston')('server');
+
+app.use(morgan('tiny', { stream: winston.stream }));
+
 const adapter = require('./index');
 
-// response
 app.use(adapter({
   caldavRoot: '/'
 }));
@@ -12,4 +16,4 @@ app.use((ctx) => {
   ctx.body = 'outside caldav server';
 });
 
-app.listen(3001);
+app.listen(3001, () => winston.debug('Server started'));
