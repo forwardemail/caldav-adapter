5a5a6e6fefe13f86fee5678b835f25efb951123f
Sanders DeNardi <sedenardi@gmail.com>
2018-08-14 15:35:53 -0400

    fix return correct object

--- routes/calendar/calendar/calendar-query.js
+++ routes/calendar/calendar/calendar-query.js
@@ -6,14 +6,14 @@ module.exports = function(opts) {
   const eventResponse = require('./eventResponse')(opts);
   return async function(ctx, calendar) {
     const filters = _.get(ctx.request.xml, 'B:calendar-query.B:filter[0].B:comp-filter');
-    if (!filters) { return; }
+    if (!filters) { return { responses: [] }; }
     const cFilter = _.find(filters, (f) => _.get(f, '$.name') === 'VCALENDAR');
-    if (!cFilter) { return; }
+    if (!cFilter) { return { responses: [] }; }
     const eFilter = _.find(cFilter['B:comp-filter'], (f) => _.get(f, '$.name') === 'VEVENT');
-    if (!eFilter) { return; }
+    if (!eFilter) { return { responses: [] }; }
     /* https://tools.ietf.org/html/rfc4791#section-9.9 */
     const timeRange = eFilter['B:time-range'];
-    if (!timeRange || !timeRange[0]) { return; }
+    if (!timeRange || !timeRange[0]) { return { responses: [] }; }
     const start = timeRange[0].$.start ? moment(timeRange[0].$.start).unix() : null;
     const end = timeRange[0].$.end ? moment(timeRange[0].$.end).unix() : null;
     const events = await opts.getEventsByDate(ctx.state.params.userId, calendar.calendarId, start, end);
