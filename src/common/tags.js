f2e275ce7d97c48bb17a4d9f388636cf2a5cc3bc
Sanders DeNardi <sedenardi@gmail.com>
2018-11-02 14:20:56 -0400

    finish principal propfind

--- src/common/tags.js
+++ src/common/tags.js
@@ -6,6 +6,8 @@ const buildTag = (namespaceURI, localName) => {
 
 const dav = 'DAV:';
 const cal = 'urn:ietf:params:xml:ns:caldav';
+const cs = 'http://calendarserver.org/ns/';
+const ical = 'http://apple.com/ns/ical/';
 const href = (url) => { return { [buildTag(dav, 'href')]: url }; };
 
 module.exports = function(opts) {
@@ -14,7 +16,7 @@ module.exports = function(opts) {
     [dav]: {
       'current-user-principal': {
         doc: 'https://tools.ietf.org/html/rfc5397#section-3',
-        resp: async (ctx) => {
+        resp: async ({ ctx }) => {
           return {
             [buildTag(dav, 'current-user-principal')]: href(ctx.state.principalUrl)
           };
@@ -22,15 +24,18 @@ module.exports = function(opts) {
       },
       'displayname': {
         doc: 'https://tools.ietf.org/html/rfc4918#section-15.2',
-        resp: async (ctx) => {
+        resp: async ({ ctx }) => {
           return {
             [buildTag(dav, 'displayname')]: ctx.state.user.principalName
           };
         }
       },
+      'getcontenttype': {
+        doc: 'https://tools.ietf.org/html/rfc2518#section-13.5'
+      },
       'principal-collection-set': {
         doc: 'https://tools.ietf.org/html/rfc3744#section-5.8',
-        resp: async (ctx) => {
+        resp: async ({ ctx }) => {
           return {
             [buildTag(dav, 'principal-collection-set')]: href(ctx.state.principalRootUrl)
           };
@@ -38,25 +43,40 @@ module.exports = function(opts) {
       },
       'principal-URL': {
         doc: 'https://tools.ietf.org/html/rfc3744#section-4.2',
-        resp: async (ctx) => {
+        resp: async ({ ctx }) => {
           return {
             [buildTag(dav, 'principal-URL')]: href(ctx.state.principalUrl)
           };
+        }
+      },
+      'resource-id': {
+        doc: 'https://tools.ietf.org/html/rfc5842#section-3.1'
+      },
+      'supported-report-set': {
+        doc: 'https://tools.ietf.org/html/rfc3253#section-3.1.5'
+      },
+      'sync-token': {
+        doc: 'https://tools.ietf.org/html/rfc6578#section-3',
+        resp: async () => {
+          return null;
         }
       }
     },
     [cal]: {
       'calendar-home-set': {
         doc: 'https://tools.ietf.org/html/rfc4791#section-6.2.1',
-        resp: async (ctx) => {
+        resp: async ({ ctx }) => {
           return {
             [buildTag(cal, 'calendar-home-set')]: href(ctx.state.calendarHomeUrl)
           };
         }
       },
+      'calendar-user-address-set': {
+        doc: 'https://tools.ietf.org/html/rfc6638#section-2.4.1'
+      },
       'schedule-inbox-URL': {
         doc: 'https://tools.ietf.org/html/rfc6638#section-2.2',
-        resp: async (/*ctx*/) => {
+        resp: async () => {
           return {
             [buildTag(cal, 'schedule-inbox-URL')]: href('')
           };
@@ -64,23 +84,38 @@ module.exports = function(opts) {
       },
       'schedule-outbox-URL': {
         doc: 'https://tools.ietf.org/html/rfc6638#section-2.1',
-        resp: async (/*ctx*/) => {
+        resp: async () => {
           return {
             [buildTag(cal, 'schedule-outbox-URL')]: href('')
           };
         }
       }
+    },
+    [cs]: {
+      'checksum-versions': {},
+      'dropbox-home-URL': {},
+      'email-address-set': {},
+      'notification-URL': {
+        doc: 'https://github.com/apple/ccs-calendarserver/blob/master/doc/Extensions/caldav-notifications.txt'
+      }
     }
   };
-  const getResponse = async (child, ctx) => {
+  const getResponse = async ({ child, ctx }) => {
+    if (!child.namespaceURI) { return null; }
     if (!tags[child.namespaceURI]) {
       log.debug(`Namespace miss: ${child.namespaceURI}`);
       return null;
     }
     const tagAction = tags[child.namespaceURI][child.localName];
-    log.debug(`Tag ${tagAction ? 'hit' : 'miss'}: ${buildTag(child.namespaceURI, child.localName)}`);
-    if (!tagAction || !tagAction.resp) { return null; }
-    return await tagAction.resp(ctx);
+    if (!tagAction) {
+      log.debug(`Tag miss: ${buildTag(child.namespaceURI, child.localName)}`);
+      return null;
+    }
+    if (!tagAction.resp) {
+      log.debug(`Tag no response: ${buildTag(child.namespaceURI, child.localName)}`);
+      return null;
+    }
+    return await tagAction.resp({ ctx });
   };
   return { tags, getResponse };
 };
--- src/routes/principal/propfind.js
+++ src/routes/principal/propfind.js
@@ -4,30 +4,12 @@ const _ = require('lodash');
 
 module.exports = function(opts) {
   const tags = require('../../common/tags')(opts);
-  const tagActions = {
-    // 'addressbook-home-set': () => '',
-    /* https://tools.ietf.org/html/rfc6638#section-2.4.1 */
-    // 'calendar-user-address-set': () => '',
-    // 'checksum-versions': () => '',
-    // 'directory-gateway': () => '',
-    // 'email-address-set': () => '',
-    /* https://tools.ietf.org/html/rfc2518#section-13.5 */
-    // 'getcontenttype': () => '',
-    /* https://github.com/apple/ccs-calendarserver/blob/master/doc/Extensions/caldav-notifications.txt */
-    // 'notification-URL': () => '',
-    /* https://tools.ietf.org/html/rfc5842#section-3.1 */
-    // 'resource-id': () => ''
-    /* https://tools.ietf.org/html/rfc3253#section-3.1.5 */
-    // 'supported-report-set': () => '',
-    /* https://tools.ietf.org/html/rfc6578#section-3 */
-    // 'sync-token': () => '<d:sync-token>555</d:sync-token>',
-  };
   return async function(ctx) {
     const propNode = xml.get('/D:propfind/D:prop', ctx.request.xml);
     const children = propNode[0] ? propNode[0].childNodes : [];
 
     const actions = _.map(children, async (child) => {
-      return await tags.getResponse(child, ctx);
+      return await tags.getResponse({ child, ctx });
     });
     const res = await Promise.all(actions);
     
