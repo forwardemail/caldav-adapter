a1d86e8a9f7a81f4fef3373a23a4574c90fc5c95
titanism <101466223+titanism@users.noreply.github.com>
2025-03-18 20:26:31 -0500

    fix: added try/catch around body parsers

--- common/parse-body.js
+++ common/parse-body.js
@@ -2,12 +2,23 @@ const raw = require('raw-body');
 const { DOMParser } = require('@xmldom/xmldom');
 
 module.exports = async function (ctx) {
-  ctx.request.body = await raw(ctx.req, {
-    encoding: true,
-    limit: '1mb' // Practical
-  });
+  try {
+    ctx.request.body = await raw(ctx.req, {
+      encoding: true,
+      limit: '10mb' // Practical
+    });
+  } catch (err) {
+    // <https://github.com/stream-utils/raw-body/blob/f62e660e7c50891844f5615de075ab145c1f6129/README.md?plain=1#L82-L116>
+    if (ctx?.app?.emit) ctx.app.emit('error', err);
+    else throw err;
+  }
 
   if (ctx.request.type.includes('xml')) {
-    ctx.request.xml = new DOMParser().parseFromString(ctx.request.body);
+    try {
+      ctx.request.xml = new DOMParser().parseFromString(ctx.request.body);
+    } catch (err) {
+      if (ctx?.app?.emit) ctx.app.emit('error', err);
+      else throw err;
+    }
   }
 };
